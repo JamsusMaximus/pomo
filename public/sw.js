f7e80452090132134c467dbc67b164ef21fd088b
JamsusMaximus <hello@jamesmcaulay.co.uk>
2025-10-23 14:58:59 +0100

    chore: Remove debug console.log statements
    - Remove PWA service worker registration logs
    - Remove service worker self-destruct logs
    - Remove user sync and Convex logs
    - Keep only error logs for actual problems
    
    Reduces console noise in development and production.

--- public/sw.js
+++ public/sw.js
@@ -4,43 +4,27 @@
  * to fix production outage caused by broken fetch handler
  */
 
-console.log("[SW EMERGENCY] Self-destruct service worker loaded");
-
 // Install: Immediately skip waiting and activate
 self.addEventListener("install", () => {
-  console.log("[SW EMERGENCY] Installing self-destruct worker");
   self.skipWaiting();
 });
 
 // Activate: Unregister self and clear all caches
 self.addEventListener("activate", (event) => {
-  console.log("[SW EMERGENCY] Activating - will self-destruct");
-
   event.waitUntil(
     Promise.all([
       // Clear all caches
       caches.keys().then((cacheNames) => {
-        console.log("[SW EMERGENCY] Deleting", cacheNames.length, "caches");
-        return Promise.all(
-          cacheNames.map((cacheName) => {
-            console.log("[SW EMERGENCY] Deleting cache:", cacheName);
-            return caches.delete(cacheName);
-          })
-        );
+        return Promise.all(cacheNames.map((cacheName) => caches.delete(cacheName)));
       }),
       // Claim all clients so we take over immediately
       self.clients.claim(),
     ]).then(() => {
-      console.log("[SW EMERGENCY] Caches cleared, now unregistering self");
-
       // Unregister this service worker
       self.registration.unregister().then(() => {
-        console.log("[SW EMERGENCY] ✅ Successfully unregistered");
-
         // Tell all clients to reload
         self.clients.matchAll().then((clients) => {
           clients.forEach((client) => {
-            console.log("[SW EMERGENCY] Telling client to reload");
             client.postMessage({ type: "SW_UNREGISTERED", message: "Reload required" });
           });
         });
